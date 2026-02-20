fb7fa6228864862534db89660fc1018f05b6c4c4
kingjohneycodey <kvngjohnny10@gmail.com>
2026-02-20 10:37:47 +0100

    fixed issues

--- lib/main.dart
+++ lib/main.dart
@@ -32,63 +32,6 @@ void main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-final _router = GoRouter(
-  routes: [
-    GoRoute(path: '/', builder: (context, state) => const SplashScreen()),
-    GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
-    GoRoute(path: '/signin', builder: (context, state) => const SignInScreen()),
-
-    GoRoute(
-      path: '/hostels',
-      builder: (context, state) => const HostelListScreen(),
-    ),
-
-    GoRoute(
-      path: '/hostel/:id',
-      builder: (context, state) {
-        final id = state.pathParameters['id']!;
-        final data = state.extra as Map<String, dynamic>?;
-        return HostelDetailScreen(hostelId: id, hostelData: data);
-      },
-    ),
-
-    GoRoute(
-      path: '/hostel/:id/rooms',
-      builder: (context, state) {
-        final id = state.pathParameters['id']!;
-        return RoomSelectionScreen(hostelId: id);
-      },
-    ),
-
-    GoRoute(path: '/complaints', builder: (context, state) => ComplaintPage()),
-
-    GoRoute(path: '/reviews', builder: (context, state) => ReviewPage()),
-
-    GoRoute(path: '/feedback', builder: (context, state) => FeedbackScreen()),
-    GoRoute(
-      path: '/admin/rooms',
-      builder: (context, state) => const ManageRoomsScreen(),
-    ),
-    GoRoute(
-      path: '/admin/rooms/add',
-      builder: (context, state) => const AddEditRoomScreen(),
-    ),
-    GoRoute(
-      path: '/admin/rooms/edit/:id',
-      builder: (context, state) {
-        final id = state.pathParameters['id']!;
-        final data = state.extra as Map<String, dynamic>?;
-        return AddEditRoomScreen(roomId: id, initialData: data);
-      },
-    ),
-    //user profile route
-    GoRoute(
-      path: '/profile',
-      builder: (context, state) => const UserProfileScreen(),
-    ),
-  ],
-);
-
 class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
